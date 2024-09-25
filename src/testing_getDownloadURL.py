318b39e3becbf6786ae79d97b4e0a1e538d2f1d7
ck90works <37384111+ck90works@users.noreply.github.com>
2024-09-25 16:03:06 +0200

    original_name and json_output

--- src/testing_getDownloadURL.py
+++ src/testing_getDownloadURL.py
@@ -1,17 +1,37 @@
 import ee
 import requests
+from datetime import date
+import json
 import os
+
+# Define a update_json function
+def update_json(filename, new_data):
+    # Check if file exists
+    if os.path.exists(filename):
+        # Read existing data
+        with open(filename, 'r') as file:
+            data = json.load(file)
+    else:
+        # Create an empty dictionary if file doesn't exist
+        data = {}
+
+    # Update data with new values
+    data.update(new_data)
+
+    # Write updated data back to file
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
 
 # Initialize the Earth Engine module
 ee.Initialize(project='mi4people')
 
 # Define the coordinates
 coordinates = [
-    [-122.4481, 37.7599],
-    [-122.5012, 37.7324],
-    [-122.5391, 37.7071],
-    [-122.4904, 37.6887],
-    [-122.4376, 37.7228]
+    [15.186692656701162,44.07373482271516],
+    [15.2052320854121,44.07373482271516],
+    [15.2052320854121,44.0876693925411],
+    [15.186692656701162,44.0876693925411],
+    [15.186692656701162,44.07373482271516]
 ]
 
 # Create a polygon from the coordinates
@@ -22,32 +42,42 @@ s2_collection = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
 
 # Filter the collection
 filtered = s2_collection.filterBounds(aoi) \
-    .filterDate('2019-07-15', '2019-07-20') \
-    .sort('CLOUDY_PIXEL_PERCENTAGE') \
+    .filterDate('2019-08-01', '2019-08-31') \
+    .sort('system:time_start', False) \
     .first()
 
-# Select the RGB bands and scale them
-rgb = filtered.select(['B4', 'B3', 'B2']) \
-    .multiply(0.0001) \
-    .clip(aoi)
+image_id = filtered.get('system:index').getInfo()
+print("image_id: ",image_id)
 
-# Set visualization parameters
-vis_params = {
-    'min': 0,
-    'max': 0.3,
-    'bands': ['B4', 'B3', 'B2']
-}
+all_bands = filtered.bandNames()
+
+# Select the RGB bands and scale them
+# rgb = filtered.select(all_bands) \
+#     .multiply(1) \
+#     .clip(aoi)
+
 
 # Set the export parameters
 export_params = {
-    'scale': 10,
+    'image': filtered.select(all_bands),
+    'scale': 20,
     'region': aoi,
     'crs': 'EPSG:4326',
-    'fileFormat': 'GeoTIFF'
+    'fileFormat': 'GeoTIFF',
+    'fileNamePrefix': image_id,
+    'filePerBand': False
 }
 
+new_json_data = {f"{date.today()}": image_id}
+
+try:
+    update_json("data.json", new_json_data)
+    print(f"Data successfully updated in data.json")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
 # Get the download URL
-url = rgb.getDownloadURL(export_params)
+url = filtered.getDownloadURL(export_params)
 
 print(f"Download URL: {url}")
 
@@ -55,8 +85,10 @@ print(f"Download URL: {url}")
 response = requests.get(url)
 if response.status_code == 200:
     # The Download URL generates a Zip-File containing tiff-files of every single band selected
-    with open("sentinel2_rgb.zip", "wb") as f:
+    with open(f"{image_id}.tif", "wb") as f:
         f.write(response.content)
-    print("Image downloaded successfully as sentinel2_rgb.zip")
+    print(f"Image downloaded successfully as {image_id}.tif")
 else:
     print(f"Failed to download image. Status code: {response.status_code}")
+
+
