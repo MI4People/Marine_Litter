a249d7e939ad2f12f918099ce03b8f291e2e0102
JingWK <shadowjingjing@live.cn>
2025-02-07 19:27:46 +0100

    add upload_delete

--- src/upload.py
+++ src/upload.py
@@ -1,29 +1,27 @@
 # pip install google-cloud-storage
-# load secret from .secret
-# pip install python-dotenv
 
 import os
-from dotenv import load_dotenv
 from google.cloud import storage
 
-# Load the .secret file
-load_dotenv('.secret')
-gcp_credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-
-def upload_to_gcs(bucket_name, source_file_path, destination_blob_name):
-    
-    client = storage.Client()
-    bucket = client.bucket(bucket_name)  
-
+def upload_delete(bucket_name, source_folder, credential):
     try:
-        client = storage.Client()
+        client = storage.Client.from_service_account_json(credential)
         bucket = client.bucket(bucket_name)
-        blob = bucket.blob(destination_blob_name)
-        blob.upload_from_filename(source_file_path)
-        print(f"File {source_file_path} uploaded to {destination_blob_name} in bucket {bucket_name}")
-    
+
+        for root, dirs, files in os.walk(source_folder):
+            for file in files:
+                source_file_path = os.path.join(root, file)  
+                destination_blob = os.path.relpath(source_file_path, source_folder).replace("\\", "/")
+                total_size = os.path.getsize(source_file_path)
+                blob = bucket.blob(destination_blob)
+                blob.upload_from_filename(source_file_path)
+                if not file.endswith(".json"):
+                    os.remove(source_file_path)
+
     except Exception as e:
         print(f"Error: {e}")
 
+
 if __name__ == "__main__":
-    upload_to_gcs("marinelitter_predicted", "resources/testFile.json", "testFile.json")
+    # Change 'resources' to the folder containing your files
+    upload("marinelitter_predicted", "resources", 'D:/Arbeit/credentials.json')
